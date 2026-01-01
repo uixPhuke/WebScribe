41f743e34e8275ac8ceda8929b4eae6adaa53516
uixPhuke <uixphuke@gmail.com>
2026-01-02 01:45:40 +0530

    highlights restore

--- content/highlight.js
+++ content/highlight.js
@@ -1,3 +1,22 @@
+async function restoreHighlights() {
+  const result = await chrome.storage.local.get("highlights");
+  const highlights = result.highlights || [];
+
+  highlights
+    .filter(h => h.url === location.href)
+    .forEach(h => {
+      const bodyHTML = document.body.innerHTML;
+
+      if (bodyHTML.includes(h.text)) {
+        document.body.innerHTML = bodyHTML.replace(
+          h.text,
+          `<mark style="background: yellow">${h.text}</mark>`
+        );
+      }
+    });
+}
+restoreHighlights();
+
 document.addEventListener("mouseup", async () => {
   const selection = window.getSelection();
   if (!selection || selection.isCollapsed) return;
